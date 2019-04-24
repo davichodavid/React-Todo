292025b13bf0517396aabe7c1b9923ec98c82b50
davichodavid <davichodavid@yahoo.com>
2019-04-24 15:54:07 -0400

    Created the form without any state or functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import TodoForm from './TodoForm'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      id: 0,
+      completed: false
+    };
+  }
   // design `App` to be the parent component of your application.
+
+
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Welcome to your My App!</h2>
+        {/* <TodoList /> */}
+        {/* <Todo /> */}
+        <TodoForm />
       </div>
     );
   }
---
+++ src/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoForm = () => {
+  return (
+    <form>
+      <input placeholder="Task" />
+      <button>Add Task</button>
+    </form>
+  );
+}
+
+export default TodoForm;
